fa6d7f6f86a3f65bb37f9026ce94742ae330c86d
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2024-11-13 23:34:10 -0800

    change the version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
 import path from 'path';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import session from 'express-session';
 import fetch from 'node-fetch';
 import { fileURLToPath } from 'url';
